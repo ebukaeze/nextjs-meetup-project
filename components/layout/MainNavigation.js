554f57e2250bae77dad12c3c519d087d39abc1e3
kingbaddo <wizzyblaq94@gmail.com>
2021-11-13 19:27:36 +0100

    starting files and configs

--- components/layout/MainNavigation.js
+++ components/layout/MainNavigation.js
@@ -1,23 +1,23 @@
 import Link from 'next/link';
 
 import Logo from './logo';
-import classes from './main-navigation.module.css';
+import classes from './MainNavigation.module.css';
 
 function MainNavigation() {
   return (
     <header className={classes.header}>
       <Link href='/'>
         <a>
-          <Logo />
+          React Meetups
         </a>
       </Link>
       <nav>
         <ul>
           <li>
-            <Link href='/posts'>Posts</Link>
+            <Link href='/'>All Meetup</Link>
           </li>
           <li>
-            <Link href='/contact'>Contact</Link>
+            <Link href='/new-meetup'>Add New Meetup</Link>
           </li>
         </ul>
       </nav>
