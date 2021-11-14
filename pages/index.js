b3ccbf60311ff62245ab4667322ee74ca8d06d54
kingbaddo <wizzyblaq94@gmail.com>
2021-11-14 23:29:49 +0100

    main index page

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,34 @@
-import { Fragment } from "react";
+
+import MeetupList from "../components/meetups/MeetupList";
+
+const DUMMYDATA = [
+    {
+     title: "A first meetup",
+     image: '/china.jpg',
+     address: ' somewhere in 19, 2nd crescent dulipa ave, greenville',
+     description: 'blockchain for entrepreneurs and the future',
+    },
+     {
+     title: "A second meetup",
+     image: '/china.jpg',
+     address: 'good time hasslefare, 15th ave, mollygill crescent, sospredan',
+     description: 'The innovation for the generation unborn',
+    },
+     {
+     title: "A Third meetup",
+     image: '/china.jpg',
+     address: 'good time hasslefare, 15th ave, mollygill crescent, sospredan',
+     description: 'The innovation for the generation unborn',
+    },
+]
+
 
 const HomePage = () => {
     return (
-        <Fragment>
-          
-        </Fragment>
+        <div>
+            
+          <MeetupList meetups={DUMMYDATA}/>
+        </div>
     )
 }
 
