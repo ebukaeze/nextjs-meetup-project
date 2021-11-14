6fc4782bb9cec39a22f5af89151f095c2b13b65f
kingbaddo <wizzyblaq94@gmail.com>
2021-11-14 23:26:50 +0100

    pass list of meetups as props

--- components/meetups/MeetupList.js
+++ components/meetups/MeetupList.js
@@ -4,14 +4,15 @@ import classes from './MeetupList.module.css';
 const MeetupList = (props) => {
     return (
         <ul className={classes.list}>
-            {props.meetups.map((meetup) => {
+            {props.meetups.map((meetup) => (
                 <MeetupItems
                 key={meetup.id}
                 id={meetup.id}
                 image={meetup.image}
                 title={meetup.title}
-                address={meetup.address}></MeetupItems>
-            })}
+                address={meetup.address}
+                description={meetup.description}/>
+            ))}
             
         </ul>
     )
